b10a19bd00618114561ada4c66646f7a7bb4c30e
MedhaPallavi1 <pallavimdh2@gmail.com>
2025-07-30 21:30:45 +0530

    added the report

--- script.py
+++ script.py
@@ -2,3 +2,12 @@ weight = float(input("enter weight in kg"))
 height = float(input("enter height in m"))
 bmi = (weight)/(height**2)
 print(bmi)
+if bmi > 28:
+    print("obese")
+elif bmi > 25:
+    print("overweight")
+elif bmi > 18:
+    print("normal BMI")
+else:
+    print("underweight")
+    
